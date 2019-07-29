1704f23f1db20a50ff151e3badfc10dbeb42823c
Zsombor Gyurkovics <zsomborjoel@gmail.com>
2019-07-29 20:57:27 +0200

    change connection

--- DbConnection.java
+++ DbConnection.java
@@ -4,9 +4,8 @@ import java.sql.*;
 
 public class DbConnection {
     public static Connection getConnection (String suburl, String username,  String password) throws  SQLException {
-        String url = "jdbc:postgresql://" + suburl + "?user=" + username + "&password=" + password;
-        Connection conn = null;
-        conn = DriverManager.getConnection(url);    
+        String url = "jdbc:postgresql://" + suburl;
+        Connection conn = DriverManager.getConnection(url, username, password);    
         return conn;
     }
 }
